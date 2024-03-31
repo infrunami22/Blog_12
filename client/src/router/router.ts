a4c0ac1dd9fc45149ee9945b587879c3f67e3e79
Domi0819 <rasztikdominika@gmail.com>
2024-03-31 21:23:58 +0200

    client base added

---
+++ client/src/router/router.ts
@@ -0,0 +1,32 @@
+import { createMemoryHistory, createRouter } from 'vue-router';
+
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+
+import { LoginService } from '@/services/LoginService';
+
+const routes = [
+  { path: '/', name:"Home", component: HomeView },
+  { path: '/login', name:'Login', component: LoginView },
+]
+
+const loginService = new LoginService()
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+router.beforeEach(async (to, from) => {
+    if (
+      // make sure the user is authenticated
+      !loginService.checkLogin() &&
+      // ❗️ Avoid an infinite redirect
+      to.name !== 'Login'
+    ) {
+      // redirect the user to the login page
+      return { name: 'Login' }
+    }
+  })
+
+export default router
