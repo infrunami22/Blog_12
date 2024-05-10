50d4e4c4fa14f5fee7939660482e6f1343c1a6be
INFRUNAMI <121566276+infrunami22@users.noreply.github.com>
2024-05-10 21:37:49 +0200

    added notifications
    added websockets and notifications

--- server/server.js
+++ server/server.js
@@ -1,20 +1,26 @@
-require ('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const userRoute = require('./userRoute')
-const topicRoute = require('./topicRoute')
-const {router2} = require('./tokenRoute')
-const adminRoute = require('./adminRoute')
-app.use(express.json())
-app.use(cors())
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const http = require('http').createServer(app);
+const {initializeSocket,notifyConnectedUsers} = require('./socketRoute'); // Import the Socket.IO setup module
 
-app.use('/users',userRoute)
-app.use('/auth',router2)
-app.use('/topics',topicRoute)
-app.use('/admin',adminRoute)
+const userRoute = require('./userRoute');
+const topicRoute = require('./topicRoute');
+const { router2 } = require('./tokenRoute');
+const adminRoute = require('./adminRoute');
 
+app.use(express.json());
+app.use(cors());
 
-app.listen(5000, () => {
-    console.log("listening on port 5000")
-})
+app.use('/users', userRoute);
+app.use('/auth', router2);
+app.use('/topics', topicRoute);
+app.use('/admin', adminRoute);
+
+const io = initializeSocket(http);
+
+const PORT = process.env.PORT || 5000;
+http.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ server/socketRoute.js
@@ -0,0 +1,78 @@
+const { Server } = require("socket.io");
+const db = require('./db');
+
+const clientsMap = new Map();
+const connectedClientsMap = new Map();
+
+function initializeSocket(server) {
+    const io = new Server(server);
+
+    io.on('connection', async (socket) => {
+        console.log('A user connected:', socket.id);
+
+        socket.on('user_id', (user_id) => {
+            console.log(`User ${user_id} connected with socket ID ${socket.id}`);
+            if (user_id) {
+                if (connectedClientsMap.has(user_id)) {
+                    connectedClientsMap.get(user_id).push(socket.id);
+                } else {
+                    connectedClientsMap.set(user_id, [socket.id]);
+                }
+            }
+        });
+
+        socket.on('disconnect', () => {
+            console.log(`User disconnected with socket ID ${socket.id}`);
+            connectedClientsMap.forEach((socketIds, userId) => {
+                const index = socketIds.indexOf(socket.id);
+                if (index !== -1) {
+                    socketIds.splice(index, 1);
+                    if (socketIds.length === 0) {
+                        connectedClientsMap.delete(userId);
+                    }
+                }
+            });
+        });
+
+        try {
+            const client = db.getClient();
+            const database = client.db("Blog");
+            const favTopicsCollection = database.collection('favorite_topics');
+
+            // Fetch data from MongoDB collection
+            const cursor = favTopicsCollection.find();
+            await cursor.forEach(doc => {
+                const { topic_id, user_id } = doc;
+                if (clientsMap.has(user_id)) {
+                    clientsMap.get(topic_id).push(user_id);
+                } else {
+                    clientsMap.set(topic_id, [user_id]);
+                }
+            });
+
+            console.log('clientsMap:', clientsMap);
+        } catch (error) {
+            console.error('Error fetching data from MongoDB:', error);
+        }
+
+    });
+
+    return io;
+}
+
+async function notifyConnectedUsers(topic_id) {
+    console.log("notifyConnectedUsers called in socketRoute");
+    const user_ids = clientsMap.get(topic_id);
+    if (user_ids) {
+        user_ids.forEach(user_id => {
+            const socket_ids = connectedClientsMap.get(user_id);
+            if (socket_ids) {
+                socket_ids.forEach(socket_id => {
+                    io.to(socket_id).emit('new_post',"new post on a topic you like!", { topic_id });
+                });
+            }
+        });
+    }
+}
+
+module.exports =  {initializeSocket,notifyConnectedUsers};
